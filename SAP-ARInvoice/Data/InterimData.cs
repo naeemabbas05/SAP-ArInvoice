82a4889948d9643d05852afbc610418b0e690bd2
exd_Waheed <exd_Waheed@WIN-58V74FJ8REL>
2022-12-06 10:15:14 +0500

    sp added

---
+++ SAP-ARInvoice/Data/InterimData.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SAP_ARInvoice.Data
+{
+    public class InterimData
+    {
+        public void ArInvoice_SP()
+        {
+            try
+            {
+                //Store the connection string in the ConnectionString variable
+                string ConnectionString = @"data source=LAPTOP-ICA2LCQL\SQLEXPRESS; database=StudentDB; integrated security=SSPI";
+                //Create the SqlConnection object
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    //Create the SqlCommand object by passing the stored procedure name and connection object as parameters
+                    SqlCommand cmd = new SqlCommand("SP_NAME", connection)
+                    {
+                        //Specify the command type as Stored Procedure
+                        CommandType = CommandType.StoredProcedure
+                    };
+                    //Open the Connection
+                    connection.Open();
+                    //Execute the command i.e. Executing the Stored Procedure using ExecuteReader method
+                    //SqlDataReader requires an active and open connection
+                    SqlDataReader sdr = cmd.ExecuteReader();
+                    //Read the data from the SqlDataReader 
+                    //Read() method will returns true as long as data is there in the SqlDataReader
+                    while (sdr.Read())
+                    {
+                        //Accessing the data using the string key as index
+                        Console.WriteLine(sdr["Id"] + ",  " + sdr["Name"] + ",  " + sdr["Email"] + ",  " + sdr["Mobile"]);
+                        //Accessing the data using the integer index position as key
+                        //Console.WriteLine(sdr[0] + ",  " + sdr[1] + ",  " + sdr[2] + ",  " + sdr[3]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception Occurred: {ex.Message}");
+            }
+            Console.ReadKey();
+        }
+
+        public void ArInvoice_API()
+        {
+
+        }
+    }
+
+}
+
