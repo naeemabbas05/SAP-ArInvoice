1eab273764d011130cb1bde1e8559d356754f326
exd_Waheed <exd_Waheed@WIN-58V74FJ8REL>
2022-12-06 13:00:23 +0500

    mathods added

--- SAP-ARInvoice/Data/InterimData.cs
+++ SAP-ARInvoice/Data/InterimData.cs
@@ -1,25 +1,40 @@
-﻿using System;
+﻿using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using SAP_ARInvoice.Model.Setting;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+
+using SAPbobsCOM;
+using SAP_ARInvoice.Connection;
 
 namespace SAP_ARInvoice.Data
 {
     public class InterimData
     {
-        public void ArInvoice_SP()
+
+        private Setting _setting;
+        public InterimData(IOptions<Setting> setting)
+        {
+            _setting = setting.Value;
+        }
+
+        public void ArInvoice_SP(string SpName)
         {
             try
             {
                 //Store the connection string in the ConnectionString variable
-                string ConnectionString = @"data source=LAPTOP-ICA2LCQL\SQLEXPRESS; database=StudentDB; integrated security=SSPI";
+                string ConnectionString = _setting.DbConnection;
                 //Create the SqlConnection object
                 using (SqlConnection connection = new SqlConnection(ConnectionString))
                 {
                     //Create the SqlCommand object by passing the stored procedure name and connection object as parameters
-                    SqlCommand cmd = new SqlCommand("SP_NAME", connection)
+                    SqlCommand cmd = new SqlCommand(SpName, connection)
                     {
                         //Specify the command type as Stored Procedure
                         CommandType = CommandType.StoredProcedure
@@ -49,7 +64,39 @@ namespace SAP_ARInvoice.Data
 
         public void ArInvoice_API()
         {
+            HttpClient client = new HttpClient();
 
+            client.BaseAddress = new Uri("https://api.nationalize.io/");
+
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.GetAsync("?name=nathaniel").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var data = response.Content.ReadAsStringAsync().Result;
+
+                CountryData jsonResponse = JsonConvert.DeserializeObject<CountryData>(data);
+                var connection = new SAP_Connection();
+                if (connection.Connect() == 0)
+                {
+                    Recordset oRecordSet;
+                    oRecordSet = connection.GetCompany().GetBusinessObject(BoObjectTypes.BoRecordset);
+
+                    foreach (var item in jsonResponse.country)
+                    {
+                        SAPbobsCOM.Items items = connection.GetCompany().GetBusinessObject(BoObjectTypes.oItems);
+                        items.ItemCode = item.country_id.ToString();
+                        items.ItemName = item.probability.ToString();
+                        items.Add();
+
+                    }
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
         }
     }
 
